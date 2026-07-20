5f6dac467ff2dc99acc1f2b3c86f9a5a59322195
sm06224 <sm06224@users.noreply.github.com>
2026-07-20 08:17:29 +0000

    deploy: 723fb1c653355fe9254bba773fe2564d178a5908

---
+++ assets/__vite-browser-external-KZ7TP7R3.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-K5h24hvz.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/yorozu/sw.js', { scope: '/yorozu/' })})}
